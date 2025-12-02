c3f3333fe775b6ec3404b33a49ad54e9105bfaf4
Chillizu <chillizu@outlook.com>
2025-12-02 13:23:30 +0800

    Added item counter

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from astrbot.api.star import Context, Star, register
 from astrbot.api import logger
 import requests
 
-@register("namecheck", "Name", "一个简单的检查mc名称是否冲突的插件", "1.0.0")
+@register("mcTools", "Name", "一个mc服务器便携工具包", "1.0.0")
 class MyPlugin(Star):
     def __init__(self, context: Context):
         super().__init__(context)
@@ -25,6 +25,34 @@ class MyPlugin(Star):
             message = f"名称 {name} 已被占用, id={data['id']}"
         logger.info(message)
         yield event.plain_result(message) # 发送一条纯文本消息
+
+    @filter.command("itemcount")
+    async def helloworld(self, event: AstrMessageEvent, item: int, tar_type: str="group", raw_type: str="single", stack: int=64):
+        """这是一个 hello world 指令""" # 这是 handler 的描述，将会被解析方便用户了解插件内容。建议填写。
+        if raw_type == "single":
+            pass
+        elif raw_type == "stack":
+            item = item * stack
+        elif raw_type == "box" or raw_type == "chest":
+            item = item * stack * 27
+        elif raw_type == "largechest":
+            item = item * stack * 54
+        else:
+            pass
+
+        if tar_type == "stack":
+            message = "{:f} 个物品可以堆叠成 {:.2f} 组——".format(item, (item / stack))
+        elif tar_type == "box" or type == "chest":
+            count_box = item / stack / 27
+            message = "{:f} 个物品可以塞满 {:.2f} 个箱子~".format(item, count_box)
+        elif tar_type == "largechest":
+            count_largechest = item / stack / 54
+            message = "{:f} 个物品可以塞满 {:.2f} 个大箱子！".format(item, count_largechest)
+        else:
+            message = "命令出错啦QAQ, 正确的写法也许是这样: /itemcount [物品数量] {结果类型: stack/box/chest/largechest} {源数据类型: single/stack/box/chest/largechest} {堆叠数目}"
+
+        logger.info(message)
+        yield event.plain_result(message) # 发送一条纯文本消息        
 
     async def terminate(self):
         """可选择实现异步的插件销毁方法，当插件被卸载/停用时会调用。"""
